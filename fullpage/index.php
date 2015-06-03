f16179e9f1d2d1e645f4ea76ca8df6d25fcfb8d3
ludwig <info@ludwig-gramberg.de>
2015-06-03 15:37:15 +0200

    sample index.php

---
+++ fullpage/index.php
@@ -0,0 +1,10 @@
+<?php
+// add the following code to the top of your wordpress index.php
+
+/* begin fullpage cache */
+if(file_exists('tc-fullpage-config.php')) {
+    require_once 'tc-fullpage-config.php';
+    require_once 'wp-content/plugins/theme-cache/fullpage/functions.php';
+    tc_process_request($tc_fp_requests, $tc_fp_nocache_cookies, $tc_fp_nocache_params, $tc_fp_mysql, $tc_fp_folder);
+}
+/* endof fullpage cache */
